45cef9dacb75978d82f8261286801215df07b4c0
Tomas Kubica <tkubica12@gmail.com>
2018-07-02 15:05:12 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,24 @@
+# Create ACR
 az group create -n acr -l westeurope
 az acr create -n tomasacrdemo -g acr --sku Standard
 
-az acr build --registry tomasacrdemo --image base:1.0.1 .
+# Manual build and push
+docker build --tag tomasacrdemo.azurecr.io/base:v1 .
+az acr login -n tomasacrdemo
+docker push tomasacrdemo.azurecr.io/base:v1
+
+# Build as a service
+az acr build --registry tomasacrdemo --image base:v2 .
+
+# Automated build
+az acr build-task create \
+    --registry tomasacrdemo \
+    --name baseautobuild \
+    --image base:{{.Build.ID}} \
+    --context https://github.com/tkubica12/acrdemo-base.git \
+    --branch master \
+    --git-access-token $GIT_PAT
+
+az acr build-task run --registry tomasacrdemo --name baseautobuild
+
+az acr build-task logs --registry tomasacrdemo
